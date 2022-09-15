c640055f9f538a8771fe34d59cbc3c5aef66d68a
G.magpie <msk7529@naver.com>
2022-09-15 15:17:48 +0900

    프로젝트 파일 생성

---
+++ EventLogger/EventLogger/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  EventLogger
+//
+//  Created by on 2022/09/15.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray2
+    }
+
+
+}
+
