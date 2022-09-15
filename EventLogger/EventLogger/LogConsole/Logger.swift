905533a598cc050d6f32a2f9f89bb270c37ecbc6
G.magpie <msk7529@naver.com>
2022-09-15 18:41:00 +0900

    로그콘솔을 MainWindow에 addSubview 후 즉시 deinit 되어 액션을 받지 못하는 문제 수정 - LogConsoleVC를 싱글턴으로.

--- EventLogger/EventLogger/LogConsole/Logger.swift
+++ EventLogger/EventLogger/LogConsole/Logger.swift
@@ -17,12 +17,25 @@ final class Logger {
     }
     
     static func start(with window: UIWindow) {
-        let LogConsoleVC = LogConsoleViewController()
-        window.addSubview(LogConsoleVC.view)
-        LogConsoleVC.view.topAnchor.constraint(equalTo: window.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
-        LogConsoleVC.view.leadingAnchor.constraint(equalTo: window.leadingAnchor, constant: 10).isActive = true
-        LogConsoleVC.view.heightAnchor.constraint(equalToConstant: 100).isActive = true
-        LogConsoleVC.view.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        let logConsoleVC = LogConsoleViewController.shared  // MainWindow에 addSubview후 deinit 되지 않도록 싱글턴으로 생성
+        window.addSubview(logConsoleVC.view)
+        
+        /*
+        let topConstraint = NSLayoutConstraint(item: logConsoleVC.view as Any, attribute: .top, relatedBy: .equal, toItem: logConsoleVC.view.superview, attribute: .top, multiplier: 1, constant: 30)
+        let leadingConstraint = NSLayoutConstraint(item: logConsoleVC.view as Any, attribute: .leading, relatedBy: .equal, toItem: logConsoleVC.view.superview, attribute: .leading, multiplier: 1, constant: 10)
+        let widthConstarint = NSLayoutConstraint(item: logConsoleVC.view as Any, attribute: .width, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 50)
+        let heightConstarint = NSLayoutConstraint(item: logConsoleVC.view as Any, attribute: .height, relatedBy: .equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 100)
+        window.addConstraint(leadingConstraint)
+        window.addConstraint(topConstraint)
+
+        logConsoleVC.view.addConstraint(widthConstarint)
+        logConsoleVC.view.addConstraint(heightConstarint)
+         */
+
+        logConsoleVC.view.topAnchor.constraint(equalTo: window.safeAreaLayoutGuide.topAnchor, constant: 30).isActive = true
+        logConsoleVC.view.leadingAnchor.constraint(equalTo: window.leadingAnchor, constant: 10).isActive = true
+        logConsoleVC.view.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        logConsoleVC.view.widthAnchor.constraint(equalToConstant: 50).isActive = true
         // window.bringSubviewToFront(LogConsoleVC.view)
     }
 }
--- EventLogger/EventLogger/LogConsole/ViewController/LogConsoleViewController.swift
+++ EventLogger/EventLogger/LogConsole/ViewController/LogConsoleViewController.swift
@@ -9,12 +9,22 @@ import UIKit
 
 final class LogConsoleViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    static let shared = LogConsoleViewController()
+    
     private let testLabel: UILabel = {
         let label: UILabel = .init(frame: .zero)
         label.text = "테스트"
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    // MARK: - Life Cycle
+    
+    deinit {
+        print("deinit LogConsoleViewController")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,12 +33,20 @@ final class LogConsoleViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         
         initView()
+        // addRecognizers()
     }
-
+    
+    // MARK: - UI
+    
     private func initView() {
         view.addSubview(testLabel)
         
         testLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         testLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
+    
+    // MARK: - Helper
+
+    // MARK: - Action
+    
 }
