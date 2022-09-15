0bc4dd66f937b6dc2321005909f38e0266848723
G.magpie <msk7529@naver.com>
2022-09-15 19:16:15 +0900

    로그콘솔 팬제스처 기능 추가

--- EventLogger/EventLogger/LogConsole/ViewController/LogConsoleViewController.swift
+++ EventLogger/EventLogger/LogConsole/ViewController/LogConsoleViewController.swift
@@ -20,6 +20,24 @@ final class LogConsoleViewController: UIViewController {
         return label
     }()
     
+    private var windowSize: CGSize {
+        if let window = view.window {
+            return window.bounds.size
+        }
+        return UIScreen.main.bounds.size
+    }
+    
+    private var safeAreaInsets: UIEdgeInsets {
+        var safeAreaInsets: UIEdgeInsets = .zero
+        safeAreaInsets.top = max(20, self.view.superview?.safeAreaInsets.top ?? 0)
+        safeAreaInsets.bottom = self.view.superview?.safeAreaInsets.bottom ?? 0
+        safeAreaInsets.left = self.view.superview?.safeAreaInsets.left ?? 0
+        safeAreaInsets.right = self.view.superview?.safeAreaInsets.right ?? 0
+        return safeAreaInsets
+    }
+    
+    private var currentFrame: CGRect?
+    
     // MARK: - Life Cycle
     
     deinit {
@@ -33,7 +51,7 @@ final class LogConsoleViewController: UIViewController {
         view.translatesAutoresizingMaskIntoConstraints = false
         
         initView()
-        // addRecognizers()
+        addRecognizers()
     }
     
     // MARK: - UI
@@ -45,8 +63,40 @@ final class LogConsoleViewController: UIViewController {
         testLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
     }
     
+    private func calculateRightViewPosition(from pos: CGPoint) -> CGPoint {
+        // 로그콘솔이 화면 밖을 벗어나지 못하도록 위치를 재조정
+        var resultPos = pos
+        let safeAreaInsets = safeAreaInsets
+
+        resultPos.x = max(pos.x, safeAreaInsets.left)
+        resultPos.x = min(resultPos.x, windowSize.width - 50 - safeAreaInsets.right)
+        resultPos.y = max(pos.y, safeAreaInsets.top)
+        resultPos.y = min(resultPos.y, windowSize.height - safeAreaInsets.bottom - 100)
+        return resultPos
+    }
+    
     // MARK: - Helper
+    
+    private func addRecognizers() {
+        view.addGestureRecognizer(UIPanGestureRecognizer(target: self, action: #selector(didReceivePanAction(_:))))
+    }
 
     // MARK: - Action
     
+    @objc
+    private func didReceivePanAction(_ sender: UIPanGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            currentFrame = view.frame
+        case .changed:
+            guard let currentFrame = currentFrame else { return }
+            
+            let offset = sender.translation(in: view.superview)
+            var destFrame = currentFrame.offsetBy(dx: offset.x, dy: offset.y)
+            destFrame.origin = calculateRightViewPosition(from: destFrame.origin)
+            view.frame = destFrame
+        default:
+            return
+        }
+    }
 }
